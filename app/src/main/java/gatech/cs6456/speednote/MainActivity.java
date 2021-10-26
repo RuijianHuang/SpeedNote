4e80fcdc1bcc59656cba6c63b8b32510bad08172
Ruijian Huang <rhuang83@gatech.edu>
2021-10-25 22:04:18 -0400

    Completed geeksforgeeks drawing app - saving functionality omitted

--- app/src/main/java/gatech/cs6456/speednote/MainActivity.java
+++ app/src/main/java/gatech/cs6456/speednote/MainActivity.java
@@ -1,20 +1,85 @@
 package gatech.cs6456.speednote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageButton;
 
 import com.google.android.material.slider.RangeSlider;
 
+import petrov.kristiyan.colorpicker.ColorPicker;
+
 public class MainActivity extends AppCompatActivity {
     private DrawView drawView;
-    private ImageButton save,color,stroke,undo;
+    private ImageButton btnSave, btnColor, btnStroke, btnUndo;
     private RangeSlider rangeSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawView = (DrawView) findViewById(R.id.draw_view);
+        rangeSlider = (RangeSlider) findViewById(R.id.rangebar);
+        btnUndo = (ImageButton) findViewById(R.id.btn_undo);
+        btnSave = (ImageButton) findViewById(R.id.btn_save);
+        btnColor = (ImageButton) findViewById(R.id.btn_color);
+        btnStroke = (ImageButton) findViewById(R.id.btn_stroke);
+
+        // TODO: how to undo (both text box and strokes)?
+        btnUndo.setOnClickListener(view -> drawView.undo());
+
+        // TODO: how to save (both text box and strokes)?
+        btnSave.setOnClickListener(view -> System.out.println("trying to save"));
+
+        // allow user to select color of the brush
+        btnColor.setOnClickListener(view -> {
+            final ColorPicker colorPicker = new ColorPicker(MainActivity.this);
+            colorPicker.setOnFastChooseColorListener(new ColorPicker.OnFastChooseColorListener() {
+                @Override
+                public void setOnFastChooseColorListener(int position, int color) {
+                    // get int value of color selected from the dialog and set as stroke color
+                    drawView.setCurrPaintColor(color);
+                }
+
+                @Override
+                public void onCancel() {
+                    colorPicker.dismissDialog();
+                }
+            })
+                .setColumns(5)                          // #color columns to show in dialog
+                .setDefaultColorButton(Color.BLACK)     // default color selected in dialog
+                .show();
+        });
+
+        // toggle visibility of the rangeSlider
+        btnStroke.setOnClickListener(view ->
+            rangeSlider.setVisibility(rangeSlider.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE)
+        );
+
+        // default values of rangeSlider
+        rangeSlider.setValueFrom(0.0f);
+        rangeSlider.setValueTo(100.0f);
+        rangeSlider.addOnChangeListener((@NonNull RangeSlider slider, float value, boolean fromUser) ->
+                drawView.setCurrStrokeWidth((int) value)
+        );
+
+        // FIXME: what is this
+        // Pass the height and width of the custom view to the innit method of the DrawView obj
+        ViewTreeObserver viewTreeObserver = drawView.getViewTreeObserver();
+        viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                drawView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                int height = drawView.getMeasuredHeight();
+                int width = drawView.getMeasuredWidth();
+                drawView.init(height, width);
+            }
+        });
     }
 }
